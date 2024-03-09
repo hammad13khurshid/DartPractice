b4e2946d26234b73b59dbd816f0b2a375fdca9cf
Hammad Khurshid Chughtaii <hammadkhurshidchughtaii@Hammads-MacBook-Pro.local>
2024-03-10 04:15:32 +0500

    This is my first commit to my dart practice repo. Added my first hello_world programm in dart

---
+++ hello_world.dart
@@ -0,0 +1,24 @@
+// This is my first ever program in dart
+
+import 'dart:io';
+
+void main() {
+  //This is the entry point of a program
+  print('Hello World.... :)');
+  //print function is used to print some output.
+
+  print("I'm Hammad Khurshid, This is my first program in dart.");
+  //To use print function, ('Example') or ("Example") should be used.
+
+  stdout.write("I'm learning Dart Programming. ");
+  //stdout.write function is used to print some output just like print function.
+  stdout.writeln("Dart Programming is fun");
+  //stdout.writeln works just like print and stdout.write but adds a new line after printing.
+}
+
+
+// Topics learned: 
+//1. Main Function.
+//2. print function. 
+//3. stdout.write function.
+//4. stdout.writeln function.
