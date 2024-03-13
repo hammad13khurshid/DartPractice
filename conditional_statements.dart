a6dca8d59cd321f68d9d8db6cc139a4e5d742f27
hammad13khurshid <hammadkhurshid931@gmail.com>
2024-03-13 23:01:09 +0500

    Learned Conditional Statements and implemented the concepts in Dart.

--- conditional_statements.dart
+++ conditional_statements.dart
@@ -1,4 +1,4 @@
-//This program demonstrates the concept of conditional statements used in Dart programming.
+// This program demonstrates conditional statements and switch statements in Dart.
 
 import 'dart:io';
 
@@ -6,72 +6,77 @@ void main() {
   print(
       "\n******************************** Conditional statements ********************************\n");
 
+  // Explain conditional statements
+  print(
+      "\nConditional statements allow you to control program flow based on certain conditions.\n");
+  print(
+      "If a condition is true, a specific block of code executes. Otherwise, another block might execute.\n\n");
+
+  // ---------- if-else statement ----------
   print("*************** if else statement ********************\n");
   print(
-      "\nConditional statements are used to execute a block of code based on some conditions.\n");
-  print(
-      "If the condition is true, then the block of code is executed. If the condition is false, then the block of code is not executed.\n\n");
+      "This statement checks a single condition and executes corresponding code.\n");
 
+  // Example program
   print("****** Example Program ******\n\n");
-
   int firstNumber = 34;
   int secondNumber = 23;
 
-  //Initialize the condition.
   if (firstNumber > secondNumber) {
     print(
-        "First number is greater than second number. \t (Condition is true, First part of the code statement is executed)\n\n");
+        "First number ($firstNumber) is greater than second number ($secondNumber).\n"); // Explain the values
   } else {
     print(
-        "Second number is greater than first number. \t (Condition is false, Second part of the code statement is executed)\n");
+        "Second number ($secondNumber) is greater than or equal to first number ($firstNumber).\n"); // Explain the values
   }
 
-  print("************** if else statement **********************\n\n");
-
+  // ---------- Nested if-else statement ----------
   print("************** nested if else statement **********************\n\n");
-
   print(
-      "Nested conditional statements are used to code multiple statements in a single statement to build a more complex logic where required");
+      "Nested if-else allows for more complex logic by checking multiple conditions within a single statement.\n");
 
+  // Example program: Voting eligibility
   print("Please enter your age to check if you are eligible to vote or not!\n");
   int myAge = int.parse(stdin.readLineSync()!);
   int allowedAge = 18;
 
   if (myAge >= allowedAge) {
     print(
-        "Your age is: $myAge and you are allowed to vote.\n PS: First statement is executed.\n\n");
+        "Your age is $myAge and you are allowed to vote.\n"); // Explain the output
   } else if (myAge == 0) {
-    print("Please enter a valid age\n PS: Second statement is executed\n\n");
+    print("Please enter a valid age.\n"); // Explain the output
   } else {
     print(
-        "Your age is: $myAge and you are not allowed to vote.\n PS: Third statement is executed.\n\n");
+        "Your age is $myAge and you are not allowed to vote yet.\n"); // Explain the output
   }
-  print("\n************** nested if else statement **********************\n\n");
 
+  // ---------- Switch statement ----------
   print(
-      "******************************** Switch Statement ********************************\n");
+      "\n******************************** Switch Statement ********************************\n");
   print(
-      "\nThe switch statement is used to execute a block of code based on the value of an expression.\n");
+      "\nA switch statement executes code based on the value of an expression compared against different cases.\n");
 
+  // Example program: Scholarship eligibility
   print("**************Example Program ******************\n");
-
   print(
-      "Please enter your CGPA to check if you are eligible for the scholorship or not!\n");
+      "Please enter your CGPA to check if you are eligible for the scholarship or not!\n");
   int cGPA = int.parse(stdin.readLineSync()!);
+
   switch (cGPA) {
     case 5:
-      print("You are top candidate for the scholorship");
+      print("You are the top candidate for the scholarship!");
       break;
     case 4:
-      print("You are eligible for the scholorship\n");
+      print("You are eligible for the scholarship.\n");
       break;
     case 3:
-      print("You are not eligible for the scholorship\n");
+      print("You are not eligible for the scholarship.\n");
       break;
     default:
-      print("Please enter a valid CGPA i.e between 2 to 5\n");
+      print("Please enter a valid CGPA between 2 and 5.\n");
       break;
   }
+
   print(
       "\n******************************** Switch Statement ********************************\n\n");
 }
